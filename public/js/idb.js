71dc566636f51c4bf49c6eb7560a07c2296f9552
Matt McGee <mlmcgeenc@gmail.com>
2022-08-19 15:28:27 -0400

    add functionality - offline transactions update when network access is restored

--- public/js/idb.js
+++ public/js/idb.js
@@ -18,7 +18,7 @@ request.onsuccess = function (e) {
 
 	// check if app is online, if yes run uploadBudget() to send all local db data to api
 	if (navigator.online) {
-    console.log('This function is not finished')
+		console.log('This function is not finished');
 	}
 };
 
@@ -37,3 +37,47 @@ function saveRecord(record) {
 	// add record to the store with add method
 	transactionObjectStore.add(record);
 }
+
+function uploadTransaction() {
+	// open a transaction on the db
+	const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+	// access the object store
+	const transactionObjectStore = transaction.objectStore('new_transaction');
+
+	// get all records from the store and assign to a variable
+	const getAll = transactionObjectStore.getAll();
+
+	// on a successful getAll()
+	getAll.onsuccess = function () {
+		// if there was data in indexedDB's store, send it to the api server
+		if (getAll.result.length > 0) {
+			fetch('/api/transaction', {
+				method: 'POST',
+				body: JSON.stringify(getAll.result),
+				headers: {
+					Accept: 'application/json, text/plain, */*',
+					'Content-Type': 'application/json',
+				},
+			})
+				.then((response) => response.json())
+				.then((response) => {
+					if (response.message) {
+						throw new Error(response);
+					}
+					// open one more transaction
+					const transaction = db.transaction(['new_transaction'], 'readwrite');
+					// access the new_transaction object store
+					const transactionObjectStore = transaction.objectStore('new_transaction');
+					// clear all items in the store
+					transactionObjectStore.clear();
+					console.log('All saved transactions submitted');
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		}
+	};
+}
+
+window.addEventListener('online', uploadTransaction);
