dae0415856ff0941af4934544dae0593c456fa86
Matt McGee <mlmcgeenc@gmail.com>
2022-08-31 12:13:53 -0400

    remove non-error reporting console.log statements

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,14 +17,11 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener('fetch', function (e) {
-	console.log('fetch request : ' + e.request.url);
 	e.respondWith(
 		caches.match(e.request).then(function (request) {
 			if (request) {
-				console.log('responding with cache : ' + e.request.url);
 				return request;
 			} else {
-				console.log('file is not cached, fetching : ' + e.request.url);
 				return fetch(e.request);
 			}
 		})
@@ -34,7 +31,6 @@ self.addEventListener('fetch', function (e) {
 self.addEventListener('install', function (e) {
 	e.waitUntil(
 		caches.open(CACHE_NAME).then(function (cache) {
-			console.log('installing cache : ' + CACHE_NAME);
 			return cache.addAll(FILES_TO_CACHE);
 		})
 	);
@@ -51,7 +47,6 @@ self.addEventListener('activate', function (e) {
 			return Promise.all(
 				keyList.map(function (key, i) {
 					if (cacheKeepList.indexOf(key) === -1) {
-						console.log('deleting cach : ' + keyList[i]);
 						return caches.delete(keyList[i]);
 					}
 				})
